ed5fe5c2845597e65fb1d225eaa22c6f9d6ece79
Koen Vlaswinkel <koen@vlaswinkel.info>
2014-09-02 16:01:57 +0200

    Fix PluginHelper

--- src/main/java/org/stevewinfield/suja/idk/game/plugins/PluginHelper.java
+++ src/main/java/org/stevewinfield/suja/idk/game/plugins/PluginHelper.java
@@ -2,18 +2,11 @@ package org.stevewinfield.suja.idk.game.plugins;
 
 import org.stevewinfield.suja.idk.Bootloader;
 import org.stevewinfield.suja.idk.game.rooms.RoomInstance;
+import org.stevewinfield.suja.idk.game.rooms.RoomItem;
 import org.stevewinfield.suja.idk.game.rooms.RoomPlayer;
 import org.stevewinfield.suja.idk.network.sessions.Session;
 
 public class PluginHelper {
-    public static Object[] getActiveSessions() {
-        return Bootloader.getSessionManager().getSessions().toArray();
-    }
-
-    public static Object[] getRoomPlayers(final RoomInstance room) {
-        return room.getRoomPlayers().values().toArray();
-	}
-	
     public static Session[] getActiveSessions() {
         return Bootloader.getSessionManager().getSessions().toArray(new Session[Bootloader.getSessionManager().getSessions().size()]);
     }
@@ -22,7 +15,7 @@ public class PluginHelper {
         return room.getRoomPlayers().values().toArray(new RoomPlayer[room.getRoomPlayers().values().size()]);
     }
 
-    public static Object[] getRoomItems(final RoomInstance room) {
-        return room.getRoomItems().values().toArray();
+    public static RoomItem[] getRoomItems(final RoomInstance room) {
+        return room.getRoomItems().values().toArray(new RoomItem[room.getRoomItems().values().size()]);
     }
 }
